e08a1aad2042467d7c4a7849642f46f11237fb72
alexve18 <alexandergud18@gmail.com>
2018-10-24 18:19:36 +0000

    test failed due to variable name changes and were fixed

--- src/Tictactoe.test.js
+++ src/Tictactoe.test.js
@@ -4,29 +4,29 @@ const Tictactoe = require('./Tictactoe');
 // Tictactoe.Tictactoe test
 it('It should call Gameboard and create an empty gameboard', () => {
     const tic = new Tictactoe();
-    for(var i = 0; i < tic.board.squares.length; i++) {
-        expect(tic.board.squares[i]).toBe(" ");
+    for(var i = 0; i < tic.gameBoard.squares.length; i++) {
+        expect(tic.gameBoard.squares[i]).toBe(" ");
     }
 });
 
 it('It should takeinput and put the X sign at the right place', () => {
     const tic = new Tictactoe();
 	expect(tic.takeInput(1)).toBe(true);
-	expect(tic.board.squares[0]).toBe("X");
+	expect(tic.gameBoard.squares[0]).toBe("X");
 });
 
 it('It should takeinput and put the O sign at the right place after x has inserted first', () => {
     const tic = new Tictactoe();
 	expect(tic.takeInput(1)).toBe(true);
-	expect(tic.board.squares[0]).toBe("X");
+	expect(tic.gameBoard.squares[0]).toBe("X");
 	expect(tic.takeInput(2)).toBe(true);
-	expect(tic.board.squares[1]).toBe("O");
+	expect(tic.gameBoard.squares[1]).toBe("O");
 });
 
 it('It should takeinput and return false because X has already claimed that square', () => {
     const tic = new Tictactoe();
 	expect(tic.takeInput(1)).toBe(true);
-	expect(tic.board.squares[0]).toBe("X");
+	expect(tic.gameBoard.squares[0]).toBe("X");
 	expect(tic.takeInput(1)).toBe(false);
-	expect(tic.board.squares[0]).toBe("X");
+	expect(tic.gameBoard.squares[0]).toBe("X");
 });
