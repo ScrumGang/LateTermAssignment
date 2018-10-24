cb31482a44449552e0cc0008ecd80d5bb9892d45
Kalman Laptop <sigurdur.kalman@hotmail.com>
2018-10-24 11:19:05 +0000

    Additional test added for GameBoard.isValidInput to test out-of-range inputs

--- src/GameBoard.test.js
+++ src/GameBoard.test.js
@@ -13,3 +13,9 @@ it('isValidInput should returns true if input is between 1 - 9', () => {
         expect(gb.isValidInput(i + 1)).toBe(true);
     }
 });
+
+it('isValidInput should returns false if input is not between 1 - 9', () => {
+    const gb = new GameBoard();
+    expect(gb.isValidInput(0)).toBe(false);
+    expect(gb.isValidInput(10)).toBe(false);
+});
