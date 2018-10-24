adba7a71b0fe87c382213bf3c4e0fe4ecc44a250
alexve18 <alexandergud18@gmail.com>
2018-10-24 18:16:05 +0000

    Tictactoe fully implemnted for console version

--- src/Tictactoe.js
+++ src/Tictactoe.js
@@ -6,14 +6,14 @@ const readline = require('readline').createInterface({
   })
 //Example constructor that sets name equal to parameter
 function Tictactoe() {
-	this.board = new GameBoard();
-	this.score = new ScoreBoard();
+	this.gameBoard = new GameBoard();
+	this.scoreBoard = new ScoreBoard();
 	this.sign = 'X';
 }
 
 Tictactoe.prototype.takeInput = function(input) {
-	if(this.board.isValidInput(input)) {
-		if(this.board.fillSquare(input, this.sign)) {
+	if(this.gameBoard.isValidInput(input)) {
+		if(this.gameBoard.fillSquare(input, this.sign)) {
 			if(this.sign == 'X') {
 				this.sign = 'O';
 			}
@@ -28,39 +28,58 @@ Tictactoe.prototype.takeInput = function(input) {
 	}
 }
 Tictactoe.prototype.showBoard = function() {
-	console.log(this.board.squares[0] + "|" + this.board.squares[1] + "|" + this.board.squares[2]);
+	console.log(this.gameBoard.squares[0] + "|" + this.gameBoard.squares[1] + "|" + this.gameBoard.squares[2]);
 	console.log("- - -");
-	console.log(this.board.squares[3] + "|" + this.board.squares[4] + "|" + this.board.squares[5]);
+	console.log(this.gameBoard.squares[3] + "|" + this.gameBoard.squares[4] + "|" + this.gameBoard.squares[5]);
 	console.log("- - -");
-	console.log(this.board.squares[6] + "|" + this.board.squares[7] + "|" + this.board.squares[8]);
+	console.log(this.gameBoard.squares[6] + "|" + this.gameBoard.squares[7] + "|" + this.gameBoard.squares[8]);
+}
+
+Tictactoe.prototype.startNewGame = function() {
+	readline.question("Would you like to start a new game? (y/n)", (input) => {
+		if(input != 'y' && input != 'n') {
+			this.startNewGame();
+		}
+		else {
+			if(input == 'y') {
+				this.sign = 'X';
+				this.gameBoard.clearBoard();
+				this.play();
+			}
+			else {
+				console.log("Goodbye");
+			}
+		}
+	})
 }
 
 Tictactoe.prototype.play = function() {
 		console.log("Scoreboard");
-		console.log("X: " + this.score.xWins + "-" + this.score.oWins + " :O");
+		console.log("X: " + this.scoreBoard.xWins + "-" + this.scoreBoard.oWins + " :O");
 		this.showBoard();
 		console.log("choose a number from 1-9");
-		readline.question("It's " + this.sign + " turn", (name) => {
-			this.takeInput(name);
-			if(!this.board.isWinner('X') && !this.board.isWinner('O') && !this.board.isFull()) {
+		readline.question("It's " + this.sign + " turn", (input) => {
+			this.takeInput(input);
+			if(!this.gameBoard.isWinner('X') && !this.gameBoard.isWinner('O') && !this.gameBoard.isFull()) {
 				this.play();
 			}
 			else {
-				if(this.board.isFull()) {
+				if(this.gameBoard.isFull()) {
 					console.log("Draw");
 				}
-				else if(this.board.isWinner('X')) {
-					this.score.raiseWins('X');
+				else if(this.gameBoard.isWinner('X')) {
+					this.scoreBoard.raiseWins('X');
 					console.log('X won!');
 				}
 				else {
-					this.score.raiseWins('O');
+					this.scoreBoard.raiseWins('O');
 					console.log('O won');
 				}
+				this.startNewGame();
 			}
 		  })
 }
 
-const tic = new Tictactoe();
-tic.play();
-//module.exports = Tictactoe;
+/*const tic = new Tictactoe();
+tic.play();*/
+module.exports = Tictactoe;
