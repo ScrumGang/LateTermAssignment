4958f771fb42082a077292ab973a485f3f7abfcd
Kalman Laptop <sigurdur.kalman@hotmail.com>
2018-10-24 11:45:46 +0000

    typos fixed in test descriptions

--- src/GameBoard.test.js
+++ src/GameBoard.test.js
@@ -1,5 +1,6 @@
 const GameBoard = require('./GameBoard');
 
+// GameBoard.Gameboard() test
 it('constructor should initialize an array of size 9, filled with whitespaces', () => {
     const gb = new GameBoard();
     for(var i = 0; i < gb.sqaures.length; i++) {
@@ -7,20 +8,21 @@ it('constructor should initialize an array of size 9, filled with whitespaces', 
     }
 });
 
-it('isValidInput should returns true if input is between 1 - 9 and corresponding square is empty', () => {
+// GameBoard.isValidinput(input) tests
+it('isValidInput should return true if input is between 1 - 9 and corresponding square is empty', () => {
     const gb = new GameBoard();
     for(var i = 0; i < gb.sqaures.length; i++) {
         expect(gb.isValidInput(i + 1)).toBe(true);
     }
 });
 
-it('isValidInput should returns false if input is not between 1 - 9', () => {
+it('isValidInput should return false if input is not between 1 - 9', () => {
     const gb = new GameBoard();
     expect(gb.isValidInput(0)).toBe(false);
     expect(gb.isValidInput(10)).toBe(false);
 });
 
-it('isValidInput should returns false if corresponding square is not empty', () => {
+it('isValidInput should return false if corresponding square is not empty', () => {
     const gb = new GameBoard();
     gb.sqaures[3] = "X";
     expect(gb.isValidInput(4)).toBe(false);
