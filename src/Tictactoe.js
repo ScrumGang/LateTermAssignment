a4d3cb885c07540e83df4d082a24a9ca24d74a16
alexve18 <alexandergud18@gmail.com>
2018-10-24 17:39:19 +0000

    Prototype of play has been implemented but it isn't talking to scoreboard and fails to check if all squares are filled

--- src/Tictactoe.js
+++ src/Tictactoe.js
@@ -1,4 +1,8 @@
 const GameBoard = require('./GameBoard');
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+  })
 //Example constructor that sets name equal to parameter
 function Tictactoe() {
 	this.board = new GameBoard();
@@ -29,6 +33,25 @@ Tictactoe.prototype.showBoard = function() {
 	console.log(this.board.squares[6] + "|" + this.board.squares[7] + "|" + this.board.squares[8]);
 }
 
+Tictactoe.prototype.play = function() {
+		this.showBoard();
+		console.log("choose a number from 1-9");
+		readline.question("It's " + this.sign + " turn", (name) => {
+			this.takeInput(name);
+			if(!this.board.isWinner('X') && !this.board.isWinner('O')) {
+				this.play();
+			}
+			else {
+				if(this.board.isWinner('X')) {
+					console.log('X won!');
+				}
+				else {
+					console.log('O won');
+				}
+			}
+		  })
+}
+
 const tic = new Tictactoe();
-tic.showBoard();
+tic.play();
 //module.exports = Tictactoe;
