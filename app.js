68e410e5b262aae79a98ca236d51947f2417f560
nokkvikarlsson <nokkvi17@ru.is>
2018-10-27 21:31:30 +0000

    Trying to get heroku to work, changes in app.js

--- app.js
+++ app.js
@@ -10,4 +10,10 @@ app.get("*", (req, res) => {
     res.status(404).send({ error: "Not found" });
 });
 
+let server = require('http').Server(app);
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+    console.log("App is running on port " + port);
+});
+
 module.exports = app;
