6a62f55dbdf1c1e1c3d97fdf2d3d762676485f5b
Kalman Laptop <sigurdur.kalman@hotmail.com>
2018-10-27 19:37:27 +0000

    basic tictactoe functionality working, aswell as resetbutton

--- src/client/index.js
+++ src/client/index.js
@@ -3,8 +3,11 @@ var GameBoard = require("../logic/GameBoard");
 var ScoreBoard = require("../logic/ScoreBoard");
 
 var squares = $(".square");
+//var squareList = $.makeArray(squares);
 var leftScoreBoard = $("#left-scoreboard");
 var rightScoreBoard = $("#right-scoreboard");
+var moveAnnouncer = $("#board th");
+var resetButton = $("#reset-button");
 var currentSymbol = "X";
 var gameOver = false;
 
@@ -13,30 +16,45 @@ var scoreBoard = new ScoreBoard();
 
 squares.on("click", function() {
     if(!gameOver) {
-        if(currentSymbol === "X" && gameBoard.fillSquare(parseInt($(this).html()), "X")) {
+        if(currentSymbol === "X" && gameBoard.fillSquare(parseInt($(this).attr("id")), "X")) {
             $(this).html(currentSymbol);
             if(gameBoard.isWinner(currentSymbol)) {
                 raiseWins("X")
             }
-            currentSymbol = "O";
+            else {
+                currentSymbol = "O";
+                moveAnnouncer.html("O, it's your turn!");
+            }
         }
-        else if(currentSymbol === "O" && gameBoard.fillSquare(parseInt($(this).html()), "O")) {
+        else if(currentSymbol === "O" && gameBoard.fillSquare(parseInt($(this).attr("id")), "O")) {
             $(this).html(currentSymbol);
             if(gameBoard.isWinner(currentSymbol)) {
                 raiseWins("O")
             }
-            currentSymbol = "X";
+            else {
+                currentSymbol = "X";
+                moveAnnouncer.html("X, it's your turn!");
+            }
         }
     }
+});
+
+resetButton.on("click", function() {
+    gameBoard.clearBoard();
+    squares.html("");
+    gameOver = false;
+    currentSymbol = "X";
 });
 
 raiseWins = function(symbol) {
     gameOver = true;
     scoreBoard.raiseWins(symbol);
     if(symbol === "X") {
+        moveAnnouncer.html("X wins!")
         leftScoreBoard.html("X has " + scoreBoard.xWins + " point/s!");
     }
     else if(symbol === "O") {
+        moveAnnouncer.html("O wins!")
         rightScoreBoard.html("O has " + scoreBoard.oWins + " point/s!");
     }
 }
