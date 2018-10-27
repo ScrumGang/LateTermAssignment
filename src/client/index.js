0abac79aa170d6ce5d40df76a94031b589adcfb6
Kalman Laptop <sigurdur.kalman@hotmail.com>
2018-10-27 20:12:38 +0000

    index.js refactored and draw functionality added

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,6 @@ var GameBoard = require("../logic/GameBoard");
 var ScoreBoard = require("../logic/ScoreBoard");
 
 var squares = $(".square");
-//var squareList = $.makeArray(squares);
 var leftScoreBoard = $("#left-scoreboard");
 var rightScoreBoard = $("#right-scoreboard");
 var moveAnnouncer = $("#board th");
@@ -15,26 +14,19 @@ var gameBoard = new GameBoard();
 var scoreBoard = new ScoreBoard();
 
 squares.on("click", function() {
-    if(!gameOver) {
-        if(currentSymbol === "X" && gameBoard.fillSquare(parseInt($(this).attr("id")), "X")) {
-            $(this).html(currentSymbol);
-            if(gameBoard.isWinner(currentSymbol)) {
-                raiseWins("X")
-            }
-            else {
-                currentSymbol = "O";
-                moveAnnouncer.html("O, it's your turn!");
-            }
+    if(!gameOver && gameBoard.fillSquare(parseInt($(this).attr("id")), currentSymbol)) {
+        $(this).html(currentSymbol);
+        if(gameBoard.squaresFilled > 4 && gameBoard.isWinner(currentSymbol)) {
+            raiseWins(currentSymbol)
         }
-        else if(currentSymbol === "O" && gameBoard.fillSquare(parseInt($(this).attr("id")), "O")) {
-            $(this).html(currentSymbol);
-            if(gameBoard.isWinner(currentSymbol)) {
-                raiseWins("O")
-            }
-            else {
-                currentSymbol = "X";
-                moveAnnouncer.html("X, it's your turn!");
-            }
+        else if(gameBoard.isFull()) {
+            setDraw();
+        }
+        else if(currentSymbol === "X") {
+            setCurrentSymbol("O")
+        }
+        else if(currentSymbol === "O") {
+            setCurrentSymbol("X");
         }
     }
 });
@@ -43,18 +35,27 @@ resetButton.on("click", function() {
     gameBoard.clearBoard();
     squares.html("");
     gameOver = false;
-    currentSymbol = "X";
+    setCurrentSymbol("X");
 });
 
 raiseWins = function(symbol) {
     gameOver = true;
     scoreBoard.raiseWins(symbol);
+    moveAnnouncer.html(symbol + " wins!")
     if(symbol === "X") {
-        moveAnnouncer.html("X wins!")
-        leftScoreBoard.html("X has " + scoreBoard.xWins + " point/s!");
+        leftScoreBoard.html(symbol + " has " + scoreBoard.getWins(symbol) + " point/s!");
     }
     else if(symbol === "O") {
-        moveAnnouncer.html("O wins!")
-        rightScoreBoard.html("O has " + scoreBoard.oWins + " point/s!");
+        rightScoreBoard.html(symbol + " has " + scoreBoard.getWins(symbol) + " point/s!");
     }
+}
+
+setDraw = function() {
+    gameOver = true;
+    moveAnnouncer.html("Draw!");
+}
+
+setCurrentSymbol = function(symbol) {
+    currentSymbol = symbol;
+    moveAnnouncer.html(symbol + ", it's your turn!");
 }
