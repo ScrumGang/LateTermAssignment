ce1a83be4245eb1d47d6073d71382881162be118
nokkvikarlsson <nokkvi17@ru.is>
2018-10-28 21:43:11 +0000

    Created more tests in interactWithGame.test.js that test toghether a full tic-tac-toe game

--- src/client/interactWithGame.test.js
+++ src/client/interactWithGame.test.js
@@ -13,12 +13,81 @@ describe("tictactoe-scrumgang.herokuapp.com", () => {
     browser.close();
   });
 
-  test("Search results found for 'Puppeteer'", async () => {
+  test("Testing tictactoe functionality", async () => {
     await page.goto(url);
 
+    //X starts the game
+    var currPlayer = await page.evaluate(() => document.querySelector('#move-announcer').innerText);
+    expect(currPlayer).toBe("X, it's your turn!");
+
+    //Test click on square 1.
     await page.click('.one');
     var x1 = await page.evaluate(() => document.querySelector('.one').innerText.substring(0, 1)); //extract the first letter because it returns extra spaces.
     expect(x1).toBe('X');
+
+    //It should be X's turn
+    var currPlayer = await page.evaluate(() => document.querySelector('#move-announcer').innerText);
+    expect(currPlayer).toBe("O, it's your turn!");
+
+    //Test click on square 2.
+    await page.click('.two');
+    var o2 = await page.evaluate(() => document.querySelector('.two').innerText.substring(0, 1)); //extract the first letter because it returns extra spaces.
+    expect(o2).toBe('O');
+
+    //Test click on square 9.
+    await page.click('.nine');
+    var x9 = await page.evaluate(() => document.querySelector('.nine').innerText.substring(0, 1)); //extract the first letter because it returns extra spaces.
+    expect(x9).toBe('X');
+
+    //Test click on square 5.
+    await page.click('.five');
+    var o5 = await page.evaluate(() => document.querySelector('.five').innerText.substring(0, 1)); //extract the first letter because it returns extra spaces.
+    expect(o5).toBe('O');
+
+    //It should be X's turn
+    var currPlayer = await page.evaluate(() => document.querySelector('#move-announcer').innerText);
+    expect(currPlayer).toBe("X, it's your turn!");
+
+    //Test click on square 8.
+    await page.click('.eight');
+    var x8 = await page.evaluate(() => document.querySelector('.eight').innerText.substring(0, 1)); //extract the first letter because it returns extra spaces.
+    expect(x8).toBe('X');
+
+    //Test click on square 6.
+    await page.click('.six');
+    var o6 = await page.evaluate(() => document.querySelector('.six').innerText.substring(0, 1)); //extract the first letter because it returns extra spaces.
+    expect(o6).toBe('O');
+
+    //X should have 0 points now.
+    var xPoints = await page.evaluate(() => document.querySelector('#left-scoreboard').innerText);
+    expect(xPoints).toBe("X has 0 point/s!");
+
+    //O should have 0 points now.
+    var oPoints = await page.evaluate(() => document.querySelector('#right-scoreboard').innerText);
+    expect(oPoints).toBe("O has 0 point/s!");
+
+    //Test click on square 7.
+    await page.click('.seven');
+    var x7 = await page.evaluate(() => document.querySelector('.seven').innerText.substring(0, 1)); //extract the first letter because it returns extra spaces.
+    expect(x7).toBe('X');
+
+    //Player X should have won by now.
+    var winner = await page.evaluate(() => document.querySelector('#move-announcer').innerText);
+    expect(winner).toBe('X wins!');
+    
+    //X should have 1 point now.
+    var xPoints = await page.evaluate(() => document.querySelector('#left-scoreboard').innerText);
+    expect(xPoints).toBe("X has 1 point/s!");
+
+    //O should have 0 points now.
+    var oPoints = await page.evaluate(() => document.querySelector('#right-scoreboard').innerText);
+    expect(oPoints).toBe("O has 0 point/s!");
+
+
+    //Start a new game
+    await page.click('#reset-button');
+    var currPlayer = await page.evaluate(() => document.querySelector('#move-announcer').innerText);
+    expect(currPlayer).toBe("X, it's your turn!");
 
 
   });
